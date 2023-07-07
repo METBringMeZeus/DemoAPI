255d97c748f8d60ac39a0d0ec22cec07f289776a
METBringMeZeus <metAnvu1507@gmail.com>
2023-07-07 13:07:59 +0700

    demo project

---
+++ demo-ResAPI/src/main/java/com/example/demoResAPI/DemoResApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoResAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoResApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoResApiApplication.class, args);
+	}
+
+}
---
+++ demo-ResAPI/src/test/java/com/example/demoResAPI/DemoResApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoResAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoResApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
